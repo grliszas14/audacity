43168118fd92ba3f5c2089aa89f49bbf665c05df
Grzegorz Wojciechowski <g.wojciechowski@mu.se>
2024-06-26 08:03:17 +0200

    [AU4] Clips actions: delete

--- au4/src/processing/internal/processingactionscontroller.cpp
+++ au4/src/processing/internal/processingactionscontroller.cpp
@@ -2,14 +2,22 @@
 * Audacity: A Digital Audio Editor
 */
 #include "processingactionscontroller.h"
+#include "project/internal/audacityproject.h"
 
 using namespace muse;
 using namespace au::processing;
 using namespace muse::async;
 using namespace muse::actions;
 
+static const ActionCode CLIP_CUT_CODE("clip-cut");
+static const ActionCode CLIP_COPY_CODE("clip-copy");
+static const ActionCode CLIP_DELETE_CODE("clip-delete");
+
 void ProcessingActionsController::init()
 {
+    dispatcher()->reg(this, CLIP_CUT_CODE, this, &ProcessingActionsController::clipCut);
+    dispatcher()->reg(this, CLIP_COPY_CODE, this, &ProcessingActionsController::clipCopy);
+    dispatcher()->reg(this, CLIP_DELETE_CODE, this, &ProcessingActionsController::clipDelete);
     dispatcher()->reg(this, "toggle-loop-region", this, &ProcessingActionsController::toggleLoopRegion);
     dispatcher()->reg(this, "clear-loop-region", this, &ProcessingActionsController::clearLoopRegion);
     dispatcher()->reg(this, "set-loop-region-to-selection", this, &ProcessingActionsController::setLoopRegionToSelection);
@@ -21,6 +29,51 @@ void ProcessingActionsController::init()
 void ProcessingActionsController::notifyActionCheckedChanged(const ActionCode& actionCode)
 {
     m_actionCheckedChanged.send(actionCode);
+}
+
+void ProcessingActionsController::clipCut()
+{
+    NOT_IMPLEMENTED;
+}
+
+void ProcessingActionsController::clipCopy()
+{
+    NOT_IMPLEMENTED;
+}
+
+void ProcessingActionsController::clipDelete()
+{
+    project::IAudacityProjectPtr project = globalContext()->currentProject();
+    auto selectedTracks = selectionController()->dataSelectedOnTracks();
+    auto selectedStartTime = selectionController()->dataSelectedStartTime();
+    auto selectedEndTime = selectionController()->dataSelectedEndTime();
+    auto tracks = project->processingProject()->trackList();
+    auto selectedClipKey = selectionController()->selectedClip();
+
+    //! TODO AU4: improve for deleting multiple selected clips
+    // remove single clip when selected via header click
+    if (selectedClipKey.index != nidx) {
+        auto clip = project->processingProject()->clipList(selectedClipKey.trackId)[selectedClipKey.index];
+        processingInteraction()->removeClip(selectedClipKey, clip.startTime, clip.endTime);
+    }
+
+    // remove multiple clips in selected region
+    for (const auto& track : tracks) {
+        if (std::find(selectedTracks.begin(), selectedTracks.end(), track.id) == selectedTracks.end()) {
+            continue;
+        }
+
+        auto clips = project->processingProject()->clipList(track.id);
+        for (const auto& clip: clips) {
+            if (selectedStartTime > clip.endTime || selectedEndTime < clip.startTime) {
+                continue;
+            }
+
+            processingInteraction()->removeClip(clip.key, selectedStartTime, selectedEndTime);
+        }
+    }
+
+    //! TODO AU4: add history of user actions
 }
 
 void ProcessingActionsController::toggleLoopRegion()
